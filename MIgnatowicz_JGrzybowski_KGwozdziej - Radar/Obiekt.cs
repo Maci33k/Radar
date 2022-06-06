7439acf9b93e919d4daaa8cb182a07dbf980b789
KGmen <106311766+KGmen@users.noreply.github.com>
2022-06-06 17:37:58 +0200

    podzielone zostaly i obiekt ruchomy prototyp

---
+++ MIgnatowicz_JGrzybowski_KGwozdziej - Radar/Obiekt.cs
@@ -0,0 +1,87 @@
+﻿namespace StatkiPowietrzne
+{
+    public class Obiekt : Punkt
+    {
+        protected double szerokosc;
+        protected double wysokosc;
+        protected double dlugosc;
+        protected double kolor; //kolor w c# ogarnac
+        protected Punkt srodek;
+        protected Trasa sciezka;//obiekt ruchomy
+
+        public Obiekt(double sz, double wys, double dl, double kolor, Punkt srd)
+        {
+            if (sz > 0)
+            {
+                this.szerokosc = sz;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            if (wys > 0)
+            {
+                this.wysokosc = wys;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            if (dl > 0)
+            {
+                this.dlugosc = dl;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            this.kolor = kolor;
+            this.srodek = srd;
+        }
+        public Obiekt(double sz, double wys, double dl, double kolor, Punkt srd, Trasa sc)
+        {
+            if (sz > 0)
+            {
+                this.szerokosc = sz;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            if (wys > 0)
+            {
+                this.wysokosc = wys;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            if (dl > 0)
+            {
+                this.dlugosc = dl;
+            }
+            else
+            {
+                throw new ArgumentException("Error");
+            }
+            this.kolor = kolor;
+            this.srodek = srd;
+            this.sciezka = sc;
+        }
+        public double GetSzerokosc()
+        {
+            return szerokosc;
+        }
+        public double GetDlugosc()
+        {
+            return dlugosc;
+        }
+        public double GetWysokosc()
+        {
+            return wysokosc;
+        }
+
+
+    }
+
+}
---
+++ MIgnatowicz_JGrzybowski_KGwozdziej - Radar/Punkt.cs
@@ -0,0 +1,28 @@
+﻿namespace StatkiPowietrzne
+{
+    public class Punkt
+    {
+        protected int x;
+        protected int y;
+
+        public Punkt()
+        {
+            x = y = 0;
+        }
+        public Punkt(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int GetX()
+        {
+            return x;
+        }
+        public int GetY()
+        {
+            return y;
+        }
+
+    }
+
+}
