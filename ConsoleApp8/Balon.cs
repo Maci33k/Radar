e1cce35e1b56017883e40166bc2f5bbef1717154
Jacek <82858@student.pb.edu.pl>
2022-06-13 20:43:43 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp8/Balon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatkiPowietrzne
+{
+    public class Balon : Statek
+    {
+        private string Znak = "@";
+        public Balon(Punkt Srodek) : base(Srodek)
+        {
+            base.Znak = Znak;
+        }
+    }
+}
---
+++ ConsoleApp8/Statek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatkiPowietrzne
+{
+    public class Statek
+    {
+        protected Punkt Srodek;
+        protected string Znak;
+        protected Punkt Koniec;
+
+        public Statek(Punkt Srodek)
+        {
+            this.Srodek = Srodek;
+            Random r = new Random();
+            Koniec = new Punkt(r.Next(0, 10), 0);
+        }
+        public Punkt GetSrodek()
+        {
+            return Srodek;
+        }
+
+
+        public string GetZnak()
+        {
+            return Znak;
+        }
+        public void SetZnak(string Znak)
+        {
+            this.Znak = Znak;
+        }
+        public Punkt GetKoniec()
+        {
+            return Koniec;
+        }
+
+
+
+    }
+}
